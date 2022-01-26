ddc4771589bdd4f4800538c832de87d06b9a996c
Timofei <t.a.maksimenko@ya.ru>
2022-01-26 17:56:23 +0300

    initial commit

---
+++ src/main/java/ru/netology/domain/NegativeIdException.java
@@ -0,0 +1,7 @@
+package ru.netology.domain;
+
+public class NegativeIdException extends RuntimeException {
+    public NegativeIdException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/netology/repositiry/ProductRepository.java
@@ -0,0 +1,59 @@
+package ru.netology.repositiry;
+
+import ru.netology.domain.NegativeIdException;
+import ru.netology.domain.NotFoundException;
+import ru.netology.domain.Product;
+
+public class ProductRepository {
+    private Product[] items = new Product[0];
+
+    public void save(Product item) {
+        int length = items.length + 1;
+        Product[] tmp = new Product[length];
+        System.arraycopy(items, 0, tmp, 0, items.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        items = tmp;
+    }
+
+    public Product[] findAll() {
+        return items;
+    }
+
+    public Product findById(int id) {
+        for (Product item : items) {
+            if (item.getId() == id) {
+                return item;
+            }
+            else {
+                throw new NotFoundException("Element with id: " + id + " not found");
+            }
+        }
+        return null;
+    }
+
+    public Product[] getAll() {
+        Product[] result = new Product[items.length];
+        for (int i = 0; i < result.length; i++) {
+            int index = items.length - i - 1;
+            result[i] = items[index];
+        }
+        return result;
+    }
+
+    public void removeById(int id) {
+        if (id<0) throw new NegativeIdException ("Element with id: " + id + " have negative volume");
+        if (findById(id) == null) throw new NotFoundException ("Element with id: " + id + " not found");
+        int length = items.length - 1;
+        Product[] tmp = new Product[length];
+        int index = 0;
+        for (Product item : items) {
+            if (item.getId() != id) {
+                tmp[index] = item;
+                index++;
+            }
+        }
+        items = tmp;
+    }
+
+}
